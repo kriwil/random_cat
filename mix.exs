a54d5b7ae7f8b8ce13d3c2bb945c29982ec18861
Aldiantoro Nugroho <kriwil@gmail.com>
2019-11-02 08:50:16 +0700

    add liveview library
    ```
    $ mix deps.get
    ```

--- mix.exs
+++ mix.exs
@@ -35,7 +35,9 @@ defmodule RandomCat.MixProject do
       {:phoenix, "~> 1.4.9"},
       {:phoenix_pubsub, "~> 1.1"},
       {:phoenix_html, "~> 2.11"},
+      {:phoenix_live_view, "~> 0.3.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:httpoison, "~> 1.6.1"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"}
